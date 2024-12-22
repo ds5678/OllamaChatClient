b740f31093574dc36edff3a5994987ad443cad11
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-12-21 20:49:44 -0800

    Add project files.

---
+++ OllamaChatClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.AI;
+
+IChatClient chatClient = new OllamaSharp.OllamaApiClient(new Uri("http://localhost:11434"), defaultModel: "llama3.2:1b");
+
+List<ChatMessage> chatMessages = [new ChatMessage(ChatRole.System, "You are a helpful assistant.")];
+while (true)
+{
+	Console.Write("You: ");
+	string? input = Console.ReadLine();
+	if (string.IsNullOrWhiteSpace(input))
+	{
+		break;
+	}
+	else
+	{
+		chatMessages.Add(new ChatMessage(ChatRole.User, input));
+		Console.WriteLine();
+	}
+
+	var completion = await chatClient.CompleteAsync(chatMessages);
+	var message = completion.Message;
+	chatMessages.Add(message);
+	Console.WriteLine($"Bot: {message.Text}");
+	Console.WriteLine();
+}
